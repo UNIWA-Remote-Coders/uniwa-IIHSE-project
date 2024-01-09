b5b78a0672442e9bc9568111e1eec136e9100aef
Grigorios Dimopoulos <gregdim13@gmail.com>
2024-01-09 15:13:21 +0200

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("localhost","root","","php_project")
+$conn = mysqli_connect("localhost:3307","root","","php_project")
         or die("Couldn't connect to database");
 
 
