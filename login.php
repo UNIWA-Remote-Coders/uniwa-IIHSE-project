52f1995653bc44e68a783aaf3b47e35e8ae759c5
Grigorios Dimopoulos <gregdim13@gmail.com>
2024-01-16 23:15:18 +0200

    a few fixes

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
   session_start();
   include('./server/connection.php');
 
-  if(isset($_SESSION['loggen_in'])){
+  if(isset($_SESSION['logged_in'])){
     header('location: account.php');
     exit;
   }
