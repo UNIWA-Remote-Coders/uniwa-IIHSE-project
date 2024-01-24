0c84f41b9763f7b553f8f897267ad264b61f85d5
Grigorios Dimopoulos <gregdim13@gmail.com>
2024-01-24 15:52:41 +0200

    Update payment.php

--- payment.php
+++ payment.php
@@ -434,6 +434,7 @@
     </script>
     
     <script src="https://www.paypalobjects.com/api/checkout.js"></script>
+
     <script>
 
       paypal.Button.render({
@@ -474,9 +475,13 @@
         }
       }, '#paypal-button');
 
+    </script>
+
+
+    <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
       crossorigin="anonymous"
-    </script>
+    ></script>
   </body>
 </html>
